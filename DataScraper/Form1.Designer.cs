bdbfb3141ac8747c0ea16fa4d7cd9676e2cc6e57
Hammad <Hammad@HAMMAD>
2017-06-17 17:55:18 +0500

    Add project files.

---
+++ DataScraper/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace DataScraper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.clickMe = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // clickMe
+            // 
+            this.clickMe.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.clickMe.Location = new System.Drawing.Point(62, 139);
+            this.clickMe.Name = "clickMe";
+            this.clickMe.Size = new System.Drawing.Size(76, 37);
+            this.clickMe.TabIndex = 1;
+            this.clickMe.Text = "Click Me";
+            this.clickMe.UseVisualStyleBackColor = false;
+            this.clickMe.Click += new System.EventHandler(this.clickMe_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.clickMe);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button clickMe;
+    }
+}
+
---
+++ DataScraper/Form1.cs
@@ -0,0 +1,87 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataScraper
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            
+            InitializeComponent();
+        }
+
+        
+
+        private void clickMe_Click(object sender, EventArgs e)
+        {
+            DateTime date = DateTime.Today;
+            MessageBox.Show("Button Clicked");
+
+            string Url = "https://www.eziline.com/";
+            HtmlWeb web = new HtmlWeb();
+            HtmlAgilityPack.HtmlDocument doc = web.Load(Url);
+
+            string Metascore = doc.DocumentNode.SelectNodes("//*[@id=\"header\"]/div[1]/div/div/div/div[1]/h2")[0].InnerText;
+            string Title= doc.DocumentNode.SelectNodes("/html/head/title")[0].InnerText;
+
+            Microsoft.Office.Interop.Excel.Application oXL;
+            Microsoft.Office.Interop.Excel._Workbook oWB;
+            Microsoft.Office.Interop.Excel._Worksheet oSheet;
+            
+
+            //Start Excel and get Application object.
+            oXL = new Microsoft.Office.Interop.Excel.Application();
+            oXL.Visible = true;
+
+            //Get a new workbook.
+            oWB = (Microsoft.Office.Interop.Excel._Workbook)(oXL.Workbooks.Add(""));
+            oSheet = (Microsoft.Office.Interop.Excel._Worksheet)oWB.ActiveSheet;
+
+            //Add table headers going cell by cell.
+            oSheet.Cells[1, 1] = "Title";
+            oSheet.Cells[1, 2] = "Data";
+            oSheet.Cells[1, 3] = "Date";
+            
+
+            //Format A1:D1 as bold, vertical alignment = center.
+            oSheet.get_Range("A1", "D1").Font.Bold = true;
+            oSheet.get_Range("A1", "D1").VerticalAlignment = Microsoft.Office.Interop.Excel.XlVAlign.xlVAlignCenter;
+
+            // Create an array to multiple values at once.
+            string[,] saNames = new string[5, 3];
+
+            saNames[0, 0] = Title;
+            saNames[0, 1] = Metascore;
+            saNames[0, 2]= date.ToString("dd/MM/yyyy");
+            
+            saNames[1, 2] = date.ToString("dd/MM/yyyy");
+
+            //Fill A2:B6 with an array of values (First and Last Names).
+            oSheet.get_Range("A2", "C6").Value2 = saNames;
+            
+            oWB.SaveAs("test505.xls", Microsoft.Office.Interop.Excel.XlFileFormat.xlWorkbookDefault, Type.Missing, Type.Missing,
+                false, false, Microsoft.Office.Interop.Excel.XlSaveAsAccessMode.xlNoChange,
+                Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
+            
+            
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string temp = "DOne";
+            MessageBox.Show(temp);
+        }
+    }
+   
+}
